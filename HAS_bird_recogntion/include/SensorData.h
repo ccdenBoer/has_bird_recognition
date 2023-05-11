cd9d543a6cc7414b9fbc95c864e59e56bb6b5a3f
Twan van Noorloos <twan.v.n@outlook.com>
2023-05-11 13:31:53 +0200

    Added methods to read data from the SEN0527 temperature and humidity sensor and necessary dependencies

--- HAS_bird_recogntion/include/SensorData.h
+++ HAS_bird_recogntion/include/SensorData.h
@@ -2,18 +2,23 @@
 #define SENSOR_DATA_H
 
 #include <DFRobot_B_LUX_V30B.h>
+#include <Adafruit_AHTX0.h>
 
 class SensorData {
     public:
-        void InitSensors(); //Method to init all the sensors
+        void InitSensors();         //Method to init all the sensors
 
-        float GetLightIntensity(); //Method which returns the light intensity of the SEN0390 light sensor
+        float GetLightIntensity();  //Method which returns the light intensity of the SEN0390 light sensor
+
+        float GetTemperature();     //Method to get the temperature from the SEN0527 T/H sensor
+        float GetHumidity();        //Method to get the humidity from the SEN0527 T/H sensor
 
         SensorData() {}
         ~SensorData(){}
 
     private:
         DFRobot_B_LUX_V30B lightSensor = DFRobot_B_LUX_V30B(1, 2, 3);
+        Adafruit_AHTX0 tempAndHumiditySensor;
 };
 
 #endif
--- HAS_bird_recogntion/src/SensorData.cpp
+++ HAS_bird_recogntion/src/SensorData.cpp
@@ -3,8 +3,31 @@
 void SensorData::InitSensors() {
     //Init light sensor
     lightSensor.begin();
+
+    //Init temperature and humidity sensor
+    if (! tempAndHumiditySensor.begin()) {
+        Serial.println("Could not find AHT? Check wiring");
+    }
 }
 
 float SensorData::GetLightIntensity() {
     return lightSensor.lightStrengthLux();
+}
+
+float SensorData::GetTemperature() {
+    Adafruit_Sensor* tempSensor = tempAndHumiditySensor.getTemperatureSensor();
+    sensors_event_t* tempEvent = nullptr;
+
+    tempSensor->getEvent(tempEvent);
+
+    return tempEvent->temperature;
+}
+
+float SensorData::GetHumidity() {
+    Adafruit_Sensor* humiditySensor = tempAndHumiditySensor.getHumiditySensor();
+    sensors_event_t* humidityEvent = nullptr;
+
+    humiditySensor->getEvent(humidityEvent);
+    
+    return humidityEvent->relative_humidity;
 }
