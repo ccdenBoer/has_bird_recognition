598bc16188220539caff534207804caca4d734d8
Twan van Noorloos <twan.v.n@outlook.com>
2023-05-11 13:34:43 +0200

    Added defines for the constructor of the light sensor

--- HAS_bird_recogntion/include/SensorData.h
+++ HAS_bird_recogntion/include/SensorData.h
@@ -4,6 +4,10 @@
 #include <DFRobot_B_LUX_V30B.h>
 #include <Adafruit_AHTX0.h>
 
+#define LIGHT_SENSOR_INPUT_PIN  1
+#define LIGHT_SENSOR_SCL_PIN    2
+#define LIGHT_SENSOR_SDA_PIN    3
+
 class SensorData {
     public:
         void InitSensors();         //Method to init all the sensors
@@ -17,7 +21,7 @@ class SensorData {
         ~SensorData(){}
 
     private:
-        DFRobot_B_LUX_V30B lightSensor = DFRobot_B_LUX_V30B(1, 2, 3);
+        DFRobot_B_LUX_V30B lightSensor = DFRobot_B_LUX_V30B(LIGHT_SENSOR_INPUT_PIN, LIGHT_SENSOR_SCL_PIN, LIGHT_SENSOR_SDA_PIN);
         Adafruit_AHTX0 tempAndHumiditySensor;
 };
 
