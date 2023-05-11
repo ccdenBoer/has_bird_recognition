407769744d99c0fde2ff3252ee7e009414f754e6
Twan van Noorloos <twan.v.n@outlook.com>
2023-05-11 16:58:24 +0200

    Added basics for the LoRaConnection class

---
+++ HAS_bird_recogntion/include/LoRaConnection.h
@@ -0,0 +1,13 @@
+#ifndef LORA_CONNECTION_H
+#define LORA_CONNECTION_H
+
+class LoRaConnection
+{
+    public:
+        LoRaConnection::LoRaConnection()    {}
+        LoRaConnection::~LoRaConnection()   {}
+
+};
+
+
+#endif
---
+++ HAS_bird_recogntion/src/LoRaConnection.cpp
@@ -0,0 +1,1 @@
+#include <LoRaConnection.h>
