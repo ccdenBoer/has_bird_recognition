996434287a669baf4436a3c03a117e3bbd930d36
Twan van Noorloos <twan.v.n@outlook.com>
2023-05-11 14:45:07 +0200

    Added method to retreive the GPS position, hasn't been tested

--- HAS_bird_recogntion/include/SensorData.h
+++ HAS_bird_recogntion/include/SensorData.h
@@ -4,13 +4,20 @@
 #include <DFRobot_B_LUX_V30B.h>
 #include <Adafruit_AHTX0.h>
 #include <Arduino.h>
+#include <Adafruit_GPS.h>
 
-#define LIGHT_SENSOR_INPUT_PIN      1
-#define LIGHT_SENSOR_SCL_PIN        2
-#define LIGHT_SENSOR_SDA_PIN        3
+#define LIGHT_SENSOR_INPUT_PIN      0
+#define LIGHT_SENSOR_SCL_PIN        0
+#define LIGHT_SENSOR_SDA_PIN        0
 
 #define RAIN_SENSOR_ANALOG_INPUT    0
 #define RAIN_SENSOR_DIGITAL_INPUT   0
+
+#define GPS_TX_PIN                  0
+#define GPS_RX_PIN                  0
+
+#define GPS_SERIAL                  Serial1
+#define GPS_ECHO                    false
 
 class SensorData {
     public:
@@ -28,12 +35,18 @@ class SensorData {
         bool    GetRainThreshold();   //Method which returns if the sensor is wet or not
         int     GetRainSurface();     //Method which returns the coverage of the rain on the sensor
 
+        //GPS sensor
+        float*    GetGPSLocation();     //Method which returns the current location of the sensor
+
         SensorData() {}
         ~SensorData(){}
 
     private:
-        DFRobot_B_LUX_V30B lightSensor = DFRobot_B_LUX_V30B(LIGHT_SENSOR_INPUT_PIN, LIGHT_SENSOR_SCL_PIN, LIGHT_SENSOR_SDA_PIN);
-        Adafruit_AHTX0 tempAndHumiditySensor;
+        DFRobot_B_LUX_V30B  lightSensor = DFRobot_B_LUX_V30B(LIGHT_SENSOR_INPUT_PIN, LIGHT_SENSOR_SCL_PIN, LIGHT_SENSOR_SDA_PIN);
+        Adafruit_AHTX0      tempAndHumiditySensor;
+
+        Adafruit_GPS        GPS     = Adafruit_GPS(&GPS_SERIAL);
+        uint32_t            timer   = millis();
 };
 
 #endif
--- HAS_bird_recogntion/src/SensorData.cpp
+++ HAS_bird_recogntion/src/SensorData.cpp
@@ -12,6 +12,16 @@ void SensorData::InitSensors() {
     //Init of rain sensor
     pinMode(RAIN_SENSOR_ANALOG_INPUT,   INPUT);
     pinMode(RAIN_SENSOR_DIGITAL_INPUT,  INPUT);
+
+    //Init GPS sensor
+    GPS.begin(9600);
+    GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+    GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); // 1 Hz update rate
+    GPS.sendCommand(PGCMD_ANTENNA);
+
+    delay(1000);
+
+    GPS_SERIAL.println(PMTK_Q_RELEASE);
 }
 
 float SensorData::GetLightIntensity() {
@@ -42,4 +52,44 @@ bool SensorData::GetRainThreshold() {
 
 int SensorData::GetRainSurface() {
     return analogRead(RAIN_SENSOR_ANALOG_INPUT);
+}
+
+float* SensorData::GetGPSLocation() {
+  char c = GPS.read();
+  float location[3];
+
+  if (GPS_ECHO) {
+    if (c) {
+        Serial.print(c);
+    }
+  }
+
+  // if a sentence is received, we can check the checksum, parse it...
+  if (GPS.newNMEAreceived()) {
+    Serial.print(GPS.lastNMEA()); // this also sets the newNMEAreceived() flag to false
+    if (!GPS.parse(GPS.lastNMEA())) { // this also sets the newNMEAreceived() flag to false
+      return location; // we can fail to parse a sentence in which case we should just wait for another
+    }
+  }
+
+    Serial.print("Date: ");
+    Serial.print(GPS.day, DEC); Serial.print('/');
+    Serial.print(GPS.month, DEC); Serial.print("/20");
+    Serial.println(GPS.year, DEC);
+
+    Serial.print("Fix: "); Serial.print((int)GPS.fix);
+    Serial.print(" quality: "); Serial.println((int)GPS.fixquality);
+
+    if (GPS.fix) {
+        Serial.print("Location: ");
+        Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
+        location[0] = GPS.latitude;
+        Serial.print(", ");
+        Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
+        location[1] = GPS.longitude;
+        Serial.print("Altitude: "); Serial.println(GPS.altitude);
+        location[2] = GPS.altitude;
+    }
+
+    return location;
 }
