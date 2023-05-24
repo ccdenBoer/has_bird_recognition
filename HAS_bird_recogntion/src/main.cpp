d53e1194e7cdcad8420c5e0a25dd37c182075a22
E_Woods <we.baars@student.avans.nl>
2023-05-24 10:47:35 +0200

    M7 platform io

--- HAS_bird_recogntion/src/main.cpp
+++ HAS_bird_recogntion/src/main.cpp
@@ -1,16 +1,46 @@
+// #include "librosa.h"
+// #include <HASFSM.h>
+// #include <NeuralNetwork.h>
 #include <Arduino.h>
-#include <HASFSM.h>
-#include <NeuralNetwork.h>
 
-NeuralNetwork nn = NeuralNetwork();
+// NeuralNetwork nn = NeuralNetwork();
+
+// void setup() {
+//   // InitHASFSM();
+//   // birdSensorFSM.setup(FSM_States::STATE_INITIALIZING, FSM_Events::EVENTS_STATE_EXECUTED);
+//   // Serial.begin(115200);
+
+
+
+// }
+
+// void loop() {
+//   // nn.InputData(nullptr, nullptr, nullptr);
+//   // birdSensorFSM.loop();
+//   delay(1000);
+//   // Serial.println("Hello World");
+
+// }
+int myLED;
 
 void setup() {
-  InitHASFSM();
-  birdSensorFSM.setup(FSM_States::STATE_INITIALIZING, FSM_Events::EVENTS_STATE_EXECUTED);
+
+   randomSeed(analogRead(0));
+
+   #ifdef CORE_CM7  
+      bootM4();  
+      myLED = LEDB; // built-in blue LED
+   #endif
+
+   #ifdef CORE_CM4  
+      myLED = LEDG; // built-in greeen LED
+   #endif   
+   pinMode(myLED, OUTPUT);
 }
 
 void loop() {
-  nn.InputData(nullptr, nullptr, nullptr);
-  birdSensorFSM.loop();
-  delay(1000);
-}
+   digitalWrite(myLED, LOW); // turn the LED on 
+   delay(200); 
+   digitalWrite(myLED, HIGH); // turn the LED off 
+   delay( rand() % 2000 + 1000); // wait for a random amount of time between 1 and 3 seconds.
+} 
