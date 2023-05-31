b8e917ce8e3753105865f6beb816b4f1237ea2eb
Lars Giskes <lars.giskes@gmail.com>
2023-05-31 12:29:46 +0200

    model on m7

--- HAS_bird_recogntion/src/m7/NeuralNetwork.cpp
+++ HAS_bird_recogntion/src/m7/NeuralNetwork.cpp
@@ -4,8 +4,9 @@
 #include "mounttest.h"
 
 NeuralNetwork::NeuralNetwork() {
+    Serial.print("Starting NN init");
     this->error_reporter = &this->micro_error_reporter;
-
+    Serial.print("error repotrer initialized");
     this->model = tflite::GetModel(mounttest_tflite);
     if (this->model->version() != TFLITE_SCHEMA_VERSION) {
         TF_LITE_REPORT_ERROR(this->error_reporter,
@@ -13,17 +14,27 @@ NeuralNetwork::NeuralNetwork() {
         "to supported version %d.\n",
         this->model->version(), TFLITE_SCHEMA_VERSION);
     }
+    Serial.print("Model loaded");
     
-    //this->resolver = tflite::MicroMutableOpResolver<1>;
+    static tflite::MicroMutableOpResolver<1> resolver;
+    resolver.AddFullyConnected();
+    resolver.AddConv2D();
+    resolver.AddMaxPool2D();
+    resolver.AddSoftmax();
+    resolver.AddRelu();
+    resolver.AddTanh();
 
-    const int tensor_arena_size = sizeof(1);//float[128][547][1]);
+    const int tensor_arena_size = 2 * sizeof(float[128][547][1]);
     uint8_t tensor_arena[tensor_arena_size];
+    Serial.print("initialized tensor arena");
 
     tflite::MicroInterpreter interpreter(model, resolver, tensor_arena, 2048, error_reporter);
     interpreter.AllocateTensors();
+    Serial.print("Allocated tensor arena");
 
     // Obtain a pointer to the model's input tensor
     input = interpreter.input(0);
+    Serial.print("Finished nn init");
 }
 
 NeuralNetwork::~NeuralNetwork()
@@ -46,5 +57,15 @@ int NeuralNetwork::ScanData() {
     }
 
     TfLiteTensor* output = interpreter.output(0);
-    return output->data.f[0];
+    float* prediction = interpreter.typed_output_tensor<float>(0);
+    float max_confidence = 0;
+    int index = -1;
+    for(int i = 0; i < output->dims->size; i++){
+        if(prediction[i] > max_confidence){
+            max_confidence = prediction[i];
+            index = i;
+        }
+    }
+    Serial.print(output->dims->size);
+    return index;
 }
--- HAS_bird_recogntion/src/main.cpp
+++ HAS_bird_recogntion/src/main.cpp
@@ -11,14 +11,15 @@
 #include <Arduino_PortentaBreakout.h>
 #include <SDCardReaderAndWriter.h>
 #include "FirmwareLoader.h"
+#include <NeuralNetwork.h>
 
 // m7 defines
 SDCardReaderAndWriter sdcard;
+NeuralNetwork nn;
+float mockdata[128][547][1];
 #endif
 
 #ifdef TARGET_PORTENTA_H7_M4
-// #include <NeuralNetwork.h>
-// NeuralNetwork nn = NeuralNetwork();
 
 int led = LEDB;
 int delay_ms = 1000;
@@ -28,17 +29,21 @@ int delay_ms = 1000;
 void setup() {
 #ifdef TARGET_PORTENTA_H7_M7
   Serial.begin(115200);
-  while (!Serial);
   setupM4Firmware();
-
   // InitHASFSM();
   // birdSensorFSM.setup(FSM_States::STATE_INITIALIZING,
   // FSM_Events::EVENTS_STATE_EXECUTED);
+   nn = NeuralNetwork();
 #endif
 RPC.begin();
 
 #ifdef TARGET_PORTENTA_H7_M4
   pinMode(led, OUTPUT);
+   // for(int x; x < 128; x++){
+   //    for(int y; y < 547; y++){
+   //       mockdata[x][y][0] = 1;
+   //    }
+   // }
 #endif
 }
 
@@ -48,6 +53,11 @@ void loop() {
   delay(1000);
 #ifdef TARGET_PORTENTA_H7_M7
   Serial.println("Hello from m7");
+    nn.InputData(mockdata);
+    int predict = nn.ScanData();
+  char buffer[100];
+  sprintf(buffer, "Predicted %d \n", predict);
+  Serial.println(buffer);
 #endif
 
 #ifdef TARGET_PORTENTA_H7_M4
@@ -55,6 +65,7 @@ void loop() {
   digitalWrite(led, HIGH);
   delay(1000);
   digitalWrite(led, LOW);
+
 #endif
 
 #ifdef TARGET_PORTENTA_H7_M7
