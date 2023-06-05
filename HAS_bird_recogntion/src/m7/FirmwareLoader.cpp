e51007e737e448bc3f86f097238db69ca424f854
E_Woods <we.baars@student.avans.nl>
2023-06-05 13:37:09 +0200

    Debug logs for tf

--- HAS_bird_recogntion/src/m7/FirmwareLoader.cpp
+++ HAS_bird_recogntion/src/m7/FirmwareLoader.cpp
@@ -1,18 +1,27 @@
 #include "FirmwareLoader.h"
-#include <Arduino.h>
-#include <FATFileSystem.h>
-#include <MBRBlockDevice.h>
-#include <PluggableUSBMSD.h>
-#include <QSPIFBlockDevice.h>
+#include <RPC.h>
 #include <SDRAM.h>
+#include <QSPIFBlockDevice.h>
+#include <MBRBlockDevice.h>
+#include <FATFileSystem.h>
+#include "PluggableUSBMSD.h"
 
 QSPIFBlockDevice root;
 mbed::MBRBlockDevice ota_data(&root, 2);
 mbed::FATFileSystem ota_data_fs("qspi");
 
-void USBMSD::begin() {}
+// void USBMSD::begin()
+// {
+// }
 
-USBMSD MassStorage(&root);
+// USBMSD MassStorage(&root);
+
+
+#ifdef TARGET_PORTENTA_H7_M4
+#define Serial RPC
+#endif
+
+#define ALIGN_PTR(p, a)   ((p & (a-1)) ?(((uintptr_t)p + a) & ~(uintptr_t)(a-1)) : p)
 
 long getFileSize(FILE *fp) {
   fseek(fp, 0, SEEK_END);
@@ -25,53 +34,71 @@ long getFileSize(FILE *fp) {
 void waitForCorrectQspi() {
   int err = ota_data_fs.mount(&ota_data);
   if (err) {
-    Serial.println("Error mounting qspi not formatted correctly");
-    while (1) {
-      delay(10000);
-    }
+	Serial.println("Error mounting qspi not formatted correctly");
+	while (1) {
+	  delay(10000);
+	}
   }
   Serial.println("QSPI correctly formatted");
-    // write small file to QSPI too see if it works
-    FILE *fp = fopen("/qspi/placeFwHere", "w");
-    if (fp == NULL) {
-      Serial.println("Error opening file");
-      while (1) {
-        delay(10000);
-      }
-    }
-    fprintf(fp, "Hello World");
-    fclose(fp);
-
-}
-
-void fwToSdram() {
-  SDRAM.begin(0);
-
-  // Copy M4 firmware to SDRAM
-  FILE *fw = fopen("/qspi/fw.bin", "r");
-  if (fw == NULL) {
-    Serial.println(
-        "Please copy a firmware for M4 core in the PORTENTA mass storage");
-    Serial.println(
-        "When done, please unmount the mass storage and reset the board");
-    MassStorage.begin();
-    while (1) {
-      delay(10000);
-    }
+  // write small file to QSPI too see if it works
+  FILE *fp = fopen("/qspi/placeFwHere", "w");
+  if (fp == nullptr) {
+	Serial.println("Error opening file");
+	while (1) {
+	  delay(10000);
+	}
   }
-  Serial.println("Copying M4 firmware to SDRAM");
-  fread((uint8_t *)CM4_BINARY_START, getFileSize(fw), 1, fw);
-  fclose(fw);
-  Serial.println("Done copying M4 firmware to SDRAM");
-
-
+  fprintf(fp, "Hello World");
+  fclose(fp);
 }
 
-/*
- * Boot M4 core
- * wait's for corretly formmated QSPI and copies the firmware to SDRAM
- */
-void setupM4Firmware() {
+
+
+tfLiteModel_t tfliteToSdram() {
+  SDRAM.begin(SDRAM_START_ADDRESS);
+  //  MPU_Config();
+  // Copy M4 firmware to SDRAM
+  FILE *fw = fopen("/qspi/model.tflite", "r");
+  if (fw == nullptr) {
+	Serial.println(
+		"Please copy a tfLite model onto the PORTENTA mass storage");
+	Serial.println(
+		"When done, please unmount the mass storage and reset the board");
+	  // MassStorage.begin();
+	while (1) {
+	  delay(10000);
+	}
+  }
+  Serial.println("Copying tflite data to SDRAM");
+  const std::size_t file_size = getFileSize(fw);
+//  print the size of the file
+  Serial.print("Size of the file: ");
+  Serial.println(file_size);
+
+  auto buffer = (uint8_t *)SDRAM.malloc(file_size + 32);
+
+  if (buffer == nullptr) {
+	Serial.println("Error allocating memory");
+  }
+  buffer = (uint8_t *)ALIGN_PTR((uintptr_t)buffer, 32);
+
+  fread(buffer, 1, file_size, fw);
+  // for (int i = 0; i < file_size; i++) {
+  //   buffer[i] = i % 256;
+  // }
+
+  fclose(fw);
+
+  Serial.println("Done copying M4 firmware to SDRAM");
+  tfLiteModel_t model;
+  model.data = buffer;
+  model.size = file_size;
+  
+  return model;
+}
+
+tfLiteModel_t loadTfliteModel() {
   waitForCorrectQspi();
-  fwToSdram();
+  auto model = tfliteToSdram();
+  return model;
 }
--- HAS_bird_recogntion/src/main.cpp
+++ HAS_bird_recogntion/src/main.cpp
@@ -2,69 +2,52 @@
 #include "RPC.h"
 #include <Arduino.h>
 
-#ifdef TARGET_PORTENTA_H7_M4
-#define Serial RPC
-#endif
-
-#ifdef TARGET_PORTENTA_H7_M7
 // m7 includes
 #include <Arduino_PortentaBreakout.h>
 #include <SDCardReaderAndWriter.h>
+
+#include <SDRAM.h>
+#include <NeuralNetwork.h>
 #include "FirmwareLoader.h"
+NeuralNetwork *nn = nullptr;
 
 // m7 defines
 SDCardReaderAndWriter sdcard;
-#endif
 
-#ifdef TARGET_PORTENTA_H7_M4
-// #include <NeuralNetwork.h>
-// NeuralNetwork nn = NeuralNetwork();
 
 int led = LEDB;
-#endif
+tfLiteModel_t model;
 
-
-void setup() {
-#ifdef TARGET_PORTENTA_H7_M7
+void setup()
+{
   Serial.begin(115200);
-  setupM4Firmware();
+  while (!Serial);
+  Serial.println("M7 booted");
 
-  // InitHASFSM();
-  // birdSensorFSM.setup(FSM_States::STATE_INITIALIZING,
-  // FSM_Events::EVENTS_STATE_EXECUTED);
-  // bootM4();
-#endif
-RPC.begin();
-
-#ifdef TARGET_PORTENTA_H7_M4
   pinMode(led, OUTPUT);
-#endif
+  model = loadTfliteModel();
+
+  char buffer[64];
+  auto len = sprintf(buffer, "The address of ptr is %x on the M7", (unsigned int)model.data);
+  Serial.write(buffer, len);
+  Serial.println();
+  for (int i = model.size - 50; i < model.size; i++)
+  {
+    Serial.print(model.data[i], HEX);
+    Serial.print(",");
+  }
+  Serial.println();
+
+  nn = new NeuralNetwork(model.data);
 }
 
-void loop() {
-  // nn.InputData(nullptr, nullptr, nullptr);
-  // birdSensorFSM.loop();
-  
-#ifdef TARGET_PORTENTA_H7_M7
+void loop()
+{
   delay(1000);
   Serial.println("Hello from m7");
-#endif
 
-#ifdef TARGET_PORTENTA_H7_M4
   delay(100);
-  Serial.println("Hello from m4");
   digitalWrite(led, HIGH);
   delay(100);
   digitalWrite(led, LOW);
-#endif
-
-#ifdef TARGET_PORTENTA_H7_M7
-  String data = "";
-  while (RPC.available()) {
-    data += (char)RPC.read();
-  }
-  if (data.length() > 0) {
-    Serial.println(data);
-  }
-#endif
 }
